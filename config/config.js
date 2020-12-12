7938c462d1d4af3d38f89690f46629dfde8aab3a
ziieng <cjengelhardt@gmail.com>
2020-12-12 11:12:25 -0800

    Initial structure provided by activity prompt

---
+++ config/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": DB_PASSWORD,
+    "database": "bantr_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+}
