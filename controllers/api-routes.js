1e72945f1f5f5c957514801a13c2e233ab45e3d4
chickey49 <camslens@gmail.com>
2020-12-12 13:43:10 -0800

    router

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,39 +1,61 @@
 // Requiring our models and passport as we've configured it
 var db = require("../models");
 var passport = require("../config/passport");
+var express = require("express");
+var router = express.Router();
 
-module.exports = function(app) {
+module.exports = function (router) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  router.post("/api/login", passport.authenticate("local"), function (req, res) {
     res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  router.post("/api/signup", function (req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
-      .then(function() {
+      .then(function () {
         res.redirect(307, "/api/login");
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.status(401).json(err);
       });
   });
 
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  router.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
 
+  router.put("/api/cats/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    cat.update(
+      {
+        sleepy: req.body.sleepy
+      },
+      condition,
+      function (result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+
+      }
+    );
+  });
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
+  router.get("/api/user_data", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -46,4 +68,25 @@ module.exports = function(app) {
       });
     }
   });
+
+  router.get("/", function (req, res) {
+    cat.all(function (data) {
+      var hbsObject = {
+        cats: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+  router.post("/api/cats", function (req, res) {
+    cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function (result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+
+
 };
+
+
