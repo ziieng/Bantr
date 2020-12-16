ab79360d7703f0e9023f6b6330c380cb20a73eb4
Amber Lee <lee.amber.alex@gmail.com>
2020-12-16 15:53:17 -0800

    commented out gravatar code on signup js, added backup folder directory.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "pass",
+    "password": "DominoL37#1",
     "database": "bantr_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
