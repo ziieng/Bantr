952b65296ac29d4144112910cef74c8c3a6bdb06
Zii Engelhardt <cjengelhardt@gmail.com>
2020-12-17 19:02:25 -0800

    Update config.js
    scrub

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "DominoL37#1",
+    "password": "pass",
     "database": "bantr_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
