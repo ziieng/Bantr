b0b70d7acbb541facc52cde7878c85bc712f2f68
ziieng <cjengelhardt@gmail.com>
2020-12-18 23:31:11 -0800

    add route for user profiles

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
-require('dotenv').config()
 var exphbs = require("express-handlebars");
 var passport = require("./config/passport");
+require('dotenv').config()
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
